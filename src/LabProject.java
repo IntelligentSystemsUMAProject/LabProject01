7266d2282cdc0c8ed09ad1435b7508cb93b3c479
Illya Rozumovskyy <panvalkon@gmail.com>
2022-04-13 10:55:50 +0200

    Dataset creation

--- src/LabProject.java
+++ src/LabProject.java
@@ -1,43 +1,78 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
 import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataPair;
 import org.encog.ml.data.basic.BasicMLDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.propagation.back.Backpropagation;
 
-
 public class LabProject {
-	
+
 	private static final double PI = Math.PI;
-	
+
 	public static void main(String[] args) {
-		
-		List<Point> training = new ArrayList<Point>();
-		List<Point> validation = new ArrayList<Point>();
-		
+
+		// List<Point> training = new ArrayList<Point>();
+		// List<Point> validation = new ArrayList<Point>();
+
+		double[][] trainingX = new double[1000][2];
+		double[][] trainingY = new double[1000][1];
+
+		double[][] validationX = new double[1000][2];
+		double[][] validationY = new double[1000][1];
+
 		for (int i = 0; i < 1000; i++) {
-			double x = Math.random() * (PI + PI) -PI;
-			double y = Math.random() * (PI + PI) -PI ;
-			training.add(new Point(x,y));
-			double x1 = Math.random() * (PI + PI) -PI;
-			double y1 = Math.random() * (PI + PI) -PI ;
-			validation.add(new Point(x1,y1)) ;
+			double x = Math.random() * (PI + PI) - PI;
+			double y = Math.random() * (PI + PI) - PI;
+			trainingX[i][0] = x;
+			trainingX[i][1] = y;
+			trainingY[i][0] = getF(x, y);
+
+			double x1 = Math.random() * (PI + PI) - PI;
+			double y1 = Math.random() * (PI + PI) - PI;
+			validationX[i][0] = x1;
+			validationX[i][1] = y1;
+			validationY[i][0] = getF(x1, y1);
 		}
-		int numOfHiddenNeurons = 1;
+		
+		/*
+		 * System.out.println("Printing an array"); for(int i = 0; i< 10; i++) {
+		 * System.out.print("X: "); for (int j = 0; j<2; j++) {
+		 * System.out.printf("%f\t",trainingX[i][j]); } System.out.printf("Y: %f\n",
+		 * trainingY[i][0]); }
+		 */
+
+		int numOfHiddenNeurons = 10;
 		BasicNetwork neuronalNetwork = new BasicNetwork();
 		neuronalNetwork.addLayer(new BasicLayer(new ActivationSigmoid(), true, 2));
 		neuronalNetwork.addLayer(new BasicLayer(new ActivationSigmoid(), true, numOfHiddenNeurons));
 		neuronalNetwork.addLayer(new BasicLayer(new ActivationSigmoid(), true, 1));
 		neuronalNetwork.getStructure().finalizeStructure();
 		neuronalNetwork.reset();
-		
 
-		// need to learn how to create a dataset;
-		Backpropagation train = new Backpropagation(neuronalNetwork, null, 0.1, 0.3);
+		MLDataSet trainingSet = new BasicMLDataSet(trainingX, trainingY);
 		
-		//System.out.println(training);	
+		Backpropagation train = new Backpropagation(neuronalNetwork, trainingSet);
+		train.iteration(50000);
+		System.out.println( train.getError());
+		System.out.println("done");
+		
+		/*
+		for(MLDataPair pair : trainingSet) {
+			MLData result = neuronalNetwork.compute(pair.getInput());
+			double[] in = pair.getInputArray();
+			System.out.printf("%f\t%f\tidl: %f\tout: %f\n", in[0], in[1], pair.getIdealArray()[0], result.getData()[0]);
+		}
+		*/
+	}
+
+	public static double getF(double x, double y) {
+		return (Math.sin(x) * Math.cos(y));
 	}
 }
